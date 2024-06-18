59e72d611a50c97baa84b3cf314248a7b51f09dc
ayamaher205 <aya.maher.m7md@gmail.com>
2024-06-18 04:16:00 +0300

    feat:customize AppBar

---
+++ lib/widgets/custom_appBar.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget{
+  final String title;
+
+  const CustomAppBar({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(title),
+      titleTextStyle: const TextStyle(fontSize: 30.0,color: Color(0xFFD22525)),
+      leading: Builder(
+        builder: (context) => IconButton(
+          icon: const Icon(Icons.menu, color: Color(0xFFD22525)),
+          onPressed: () {
+            Scaffold.of(context).openDrawer();
+          },
+        ),
+      ),
+    );
+  }
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
