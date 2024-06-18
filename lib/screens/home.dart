eb1f3f2d8f977edf1da6619d1f827539ccef8d90
ayamaher205 <aya.maher.m7md@gmail.com>
2024-06-18 04:14:05 +0300

    feat:create home page screen

---
+++ lib/screens/home.dart
@@ -0,0 +1,16 @@
+import 'package:bus_iti/widgets/bus_card.dart';
+import 'package:bus_iti/widgets/custom_drawer.dart';
+import 'package:flutter/material.dart';
+import 'package:bus_iti/widgets/custom_appBar.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: CustomAppBar(title: 'ITI'),
+      drawer: CustomDrawer(),
+      body:BusCard(),
+    );
+}
+}
