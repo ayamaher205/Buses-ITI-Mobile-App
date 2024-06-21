87f39b56a31e7066fe32923921886032d3fcdd71
ayamaher205 <aya.maher.m7md@gmail.com>
2024-06-21 19:47:57 +0300

    style:refactore height of card to be responsive

--- lib/widgets/bus_card.dart
+++ lib/widgets/bus_card.dart
@@ -1,79 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:bus_iti/screens/bus_line.dart';
 
 class BusCard extends StatelessWidget {
-  const BusCard({super.key});
+  final String title;
+  final String start;
+  final String end;
+  final String imageUrl;
+
+  const BusCard({
+    super.key,
+    required this.title,
+    required this.start,
+    required this.end,
+    required this.imageUrl,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: SizedBox(
-        height: 100.0,
-        child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15.0),
-          ),
-          elevation: 5,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        elevation: 5,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: IntrinsicHeight(
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 ClipRRect(
                   borderRadius: BorderRadius.circular(5),
-                  child: Image.network(
-                    'https://www.allstarvip.com/wp-content/uploads/2020/11/chartered-bus-sightseeing-tour.jpg',
+                  child:  Image.network(
+                    imageUrl,
                     fit: BoxFit.cover,
+                    height: 80,
+                    width: 80,
                   ),
                 ),
                 const SizedBox(width: 14.0),
-                const Expanded(
+                Expanded(
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
                     children: [
                       Text(
-                        'Title of the Card',
-                        style: TextStyle(
+                        title,
+                        style: const TextStyle(
                           fontSize: 18.0,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
-                      SizedBox(height: 6.0),
+                      const SizedBox(height: 6.0),
                       Text(
-                        'Start: 6:45 PM',
-                        style: TextStyle(fontSize: 15.0),
+                        'Morning: $start AM',
+                        style: const TextStyle(fontSize: 15.0),
                       ),
                       Text(
-                        'End: 6:45 PM',
-                        style: TextStyle(fontSize: 15.0),
+                        'Departure: $end PM',
+                        style: const TextStyle(fontSize: 15.0),
                       ),
                     ],
                   ),
                 ),
-                Container(
-                 // height: 60.0,
-                  child: Align(
-                    alignment: Alignment.center,
-                    child: TextButton(
-                      onPressed: () {
-                        print('Details button pressed');
-                      },
-                      style: ButtonStyle(
-                        foregroundColor: WidgetStateProperty.all<Color>(const Color(0xFFD22525)),
-                        textStyle: WidgetStateProperty.all<TextStyle>(
-                          const TextStyle(
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.bold,
-                          ),
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const BusLine(),
                         ),
-                        shape: WidgetStateProperty.all<OutlinedBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8.0),
-                          ),
+                      );
+                    },
+                    style: ButtonStyle(
+                      foregroundColor: WidgetStateProperty.all<Color>(
+                        const Color(0xFFD22525),
+                      ),
+                      textStyle: WidgetStateProperty.all<TextStyle>(
+                        const TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      child: const Text('Details'),
+                      shape: WidgetStateProperty.all<OutlinedBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
                     ),
+                    child: const Text('Details'),
                   ),
                 ),
               ],
