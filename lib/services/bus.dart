c5647126aab1020bd1c1b480d06691b68ca32d41
ayamaher205 <aya.maher.m7md@gmail.com>
2024-06-21 19:59:45 +0300

    feat:create sevice to get all buses

---
+++ lib/services/bus.dart
@@ -0,0 +1,20 @@
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'dart:convert';
+import 'package:bus_iti/models/bus.dart';
+class BusLines{
+  Future<List<Bus>> getBuses() async {
+    var url = Uri.parse(dotenv.env['URL']!);
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      List<dynamic> jsonResponse = json.decode(response.body);
+      List<Bus> buses = jsonResponse.map((bus) => Bus.fromJson(bus)).toList();
+      print(buses);
+      return buses;
+    } else {
+      throw Exception('Failed to load buses');
+    }
+  }
+
+}
